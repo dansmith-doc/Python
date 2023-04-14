613b8eb2879b51dceee9b1a7a0ec1cb51628980b
Dan Smith <dan@indigomail.uk>
2023-04-14 18:49:37 +0100

    File update

---
+++ Training/workingFile.py
@@ -0,0 +1,32 @@
+print('Hello World!')
+
+##Printing tabbed
+print('This is an example of a\t tabbed line')
+
+#Divide integers
+a = 15
+b = 2.25
+
+print(a // b)
+
+parrot = "Norwegian blue"
+print(parrot)
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+print()
+
+print(parrot[10:14]) #Slicing
+
+letters = 'abcdefghijkmnopqrstuvwxyz'
+
+print()
+
+print(letters[15:12:-1]) # qpo
+print(letters[4::-1])    # edcba
+print(letters[25:16:-1]) # zyxwvuts
+
