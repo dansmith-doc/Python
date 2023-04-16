ba9e196da8cdddb7006e444ca816a5826a6e343a
Dan Smith <dan@indigomail.uk>
2023-04-16 22:21:25 +0100

    Update file

--- Training/workingFile.py
+++ Training/workingFile.py
@@ -1,32 +1,41 @@
-print('Hello World!')
+# print('Hello World!')
 
-##Printing tabbed
-print('This is an example of a\t tabbed line')
+# ##Printing tabbed
+# print('This is an example of a\t tabbed line')
 
-#Divide integers
-a = 15
-b = 2.25
+# #Divide integers
+# a = 15
+# b = 2.25
 
-print(a // b)
+# print(a // b)
 
-parrot = "Norwegian blue"
-print(parrot)
-print(parrot[-11])
-print(parrot[-10])
-print(parrot[-5])
-print(parrot[-11])
-print(parrot[-8])
-print(parrot[-6])
+# parrot = "Norwegian blue"
+# print(parrot)
+# print(parrot[-11])
+# print(parrot[-10])
+# print(parrot[-5])
+# print(parrot[-11])
+# print(parrot[-8])
+# print(parrot[-6])
 
-print()
+# print()
 
-print(parrot[10:14]) #Slicing
+# print(parrot[10:14]) #Slicing Start/Stop/Step
 
-letters = 'abcdefghijkmnopqrstuvwxyz'
+# letters = 'abcdefghijkmnopqrstuvwxyz'
 
-print()
+# print()
 
-print(letters[15:12:-1]) # qpo
-print(letters[4::-1])    # edcba
-print(letters[25:16:-1]) # zyxwvuts
+# print(letters[15:12:-1]) # qpo
+# print(letters[4::-1])    # edcba
+# print(letters[25:16:-1]) # zyxwvuts
+# print(letters[:])
 
+# print("Pi is approximately {0:12}".format(22/7))
+
+age = int(input("Hpw old are you? "))
+
+if 16 <= age <= 65:
+    print("Have a good day at work")
+else:
+    print("Enjoy your free time!")
